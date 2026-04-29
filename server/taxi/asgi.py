19c6c7dc662b95bbcc328908f944199949f11fd5
Sean Callaway <seancallaway@gmail.com>
2026-04-29 11:08:22 -0700

    Add basic websocket consumer.

--- server/taxi/asgi.py
+++ server/taxi/asgi.py
@@ -1,10 +1,16 @@
 import os
 
-from channels.routing import ProtocolTypeRouter
+from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
+from django.urls import path
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'taxi.settings')
 
+from trips.consumers import TaxiConsumer
+
 application = ProtocolTypeRouter({
     'http': get_asgi_application(),
+    'websocket': URLRouter([
+        path('taxi/', TaxiConsumer.as_asgi()),
+    ])
 })
