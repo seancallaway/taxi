ba9dc7f3d1561aeae1e9050ffb0ffcf3f437faa1
Sean Callaway <seancallaway@gmail.com>
2026-04-29 12:30:47 -0700

    Add driver pool.

--- server/trips/consumers.py
+++ server/trips/consumers.py
@@ -1,25 +1,38 @@
+from channels.db import database_sync_to_async
 from channels.generic.websocket import AsyncJsonWebsocketConsumer
 
 
 class TaxiConsumer(AsyncJsonWebsocketConsumer):
     groups = ['test']
 
+    @database_sync_to_async
+    def _get_user_group(self, user):
+        return user.groups.first().name
+
     async def connect(self):
         user = self.scope['user']
         if user.is_anonymous:
             await self.close()
         else:
-            await self.channel_layer.group_add(
-                group='test',
-                channel=self.channel_name,
-            )
+            user_group = await self._get_user_group(user)
+            if user_group == 'driver':
+                await self.channel_layer.group_add(
+                    group='drivers',
+                    channel=self.channel_name,
+                )
             await self.accept()
 
     async def disconnect(self, code):
-        await self.channel_layer.group_discard(
-            group='test',
-            channel=self.channel_name,
-        )
+        user = self.scope['user']
+        if user.is_anonymous:
+            await self.close()
+        else:
+            user_group = await self._get_user_group(user)
+            if user_group == 'driver':
+                await self.channel_layer.group_discard(
+                    group='drivers',
+                    channel=self.channel_name,
+                )
         await super().disconnect(code)
 
     async def echo_message(self, msg):
--- server/trips/tests/test_websocket.py
+++ server/trips/tests/test_websocket.py
@@ -3,6 +3,7 @@ from channels.db import database_sync_to_async
 from channels.layers import get_channel_layer
 from channels.testing import WebsocketCommunicator
 from django.contrib.auth import get_user_model
+from django.contrib.auth.models import Group
 from rest_framework_simplejwt.tokens import AccessToken
 
 from taxi.asgi import application
@@ -15,11 +16,16 @@ TEST_CHANNEL_LAYERS = {
 
 
 @database_sync_to_async
-def create_user(username, password):
+def create_user(username, password, group='rider'):
     user = get_user_model().objects.create_user(
         username=username,
-        password=password
+        password=password,
     )
+
+    user_group, _ = Group.objects.get_or_create(name=group)
+    user.groups.add(user_group)
+    user.save()
+
     access = AccessToken.for_user(user)
     return user, access
 
@@ -87,6 +93,27 @@ class TestWebsocket:
         }
         channel_layer = get_channel_layer()
         await channel_layer.group_send('test', message=message)
+        response = await communicator.receive_json_from()
+        assert response == message
+        await communicator.disconnect()
+
+
+    async def test_can_join_driver_pool(self, settings):
+        settings.CHANNEL_LAYERS = TEST_CHANNEL_LAYERS
+        _, access = await create_user(
+            'test.user@example.com', 'pAssw0rd', 'driver'
+        )
+        communicator = WebsocketCommunicator(
+            application=application,
+            path=f'/taxi/?token={access}',
+        )
+        await communicator.connect()
+        message = {
+            'type': 'echo.message',
+            'data': 'This is a test message',
+        }
+        channel_layer = get_channel_layer()
+        await channel_layer.group_send('drivers', message=message)
         response = await communicator.receive_json_from()
         assert response == message
         await communicator.disconnect()
