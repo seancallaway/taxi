a9ce1ed5926f84ef7721e00e1944b8a3ffc43254
Sean Callaway <seancallaway@gmail.com>
2026-05-05 13:45:31 -0700

    Allow driver to update trip.

--- server/trips/consumers.py
+++ server/trips/consumers.py
@@ -24,6 +24,13 @@ class TaxiConsumer(AsyncJsonWebsocketConsumer):
         return serializer.create(serializer.validated_data)
 
     @database_sync_to_async
+    def _update_trip(self, data):
+        instance = Trip.objects.get(id=data.get('id'))
+        serializer = TripSerializer(data=data)
+        serializer.is_valid(raise_exception=True)
+        return serializer.update(instance, serializer.validated_data)
+
+    @database_sync_to_async
     def _get_trip_data(self, trip):
         return NestedTripSerializer(trip).data
 
@@ -94,9 +101,35 @@ class TaxiConsumer(AsyncJsonWebsocketConsumer):
     async def echo_message(self, msg):
         await self.send_json(msg)
 
+    async def update_trip(self, message):
+        data = message.get('data')
+        trip = await self._update_trip(data)
+        trip_id = f'{trip.id}'
+        trip_data = await self._get_trip_data(trip)
+
+        await self.channel_layer.group_send(
+            group=trip_id,
+            message={
+                'type': 'echo.message',
+                'data': trip_data,
+            },
+        )
+
+        await self.channel_layer.group_add(
+            group=trip_id,
+            channel=self.channel_name,
+        )
+
+        await self.send_json({
+            'type': 'echo.message',
+            'data': trip_data,
+        })
+
     async def receive_json(self, content, **kwargs):
         message_type = content.get('type')
         if message_type == 'create.trip':
             await self.create_trip(content)
+        elif message_type == 'update.trip':
+            await self.update_trip(content)
         elif message_type == 'echo.message':
             await self.echo_message(content)
